8e848eb00e932c598956623ac4f1190153e91e3e
novae <novaegr@gmail.com>
2013-11-01 13:08:04 -0600

    INital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rspec::Application.config.secret_key_base = '582d3b6f095f13c46c4bc5f979e5a5246eb3ed278e0677afe00aab6fca9213d47cd108d11facd4bcdb2eaf9d41c7af769ae9a9ba8caca3c14d146c1fab0c2304'
